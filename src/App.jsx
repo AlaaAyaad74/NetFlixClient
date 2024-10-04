50de0ca0741c358c36ca668c20bf49adbbc7ed2a
motaz_y <motazyasser84@gmail.com>
2024-10-04 13:58:53 +0300

    final first

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import AppRoutes from "./config/Routes";
 import "./App.scss";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import Header from "./coponents/utilitiesCpmponents/header/Header.jsx";
 import Footer from "./coponents/utilitiesCpmponents/footer/Footer.jsx";
+import PropType from "prop-types";
 
 // Helper component to determine if the current route is a landing page
 const LayoutWrapper = ({ children }) => {
@@ -13,7 +14,7 @@ const LayoutWrapper = ({ children }) => {
 
   useEffect(() => {
     // Define the landing page route(s)
-    const landingPageRoutes = ["/", "/landing","/login"];
+    const landingPageRoutes = ["/", "/landing", "/login"];
 
     // Set isLandingPage to true if the current path matches landing page routes
     setIsLandingPage(landingPageRoutes.includes(location.pathname));
@@ -30,6 +31,7 @@ const LayoutWrapper = ({ children }) => {
 };
 
 const App = () => {
+  localStorage.setItem("authToken", "motaz");
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
@@ -39,6 +41,10 @@ const App = () => {
       </Suspense>
     </Router>
   );
+};
+
+LayoutWrapper.propTypes = {
+  children: PropType.node,
 };
 
 export default App;

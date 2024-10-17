6193c98adc0a1b124e80530bb9f7dbd8e399c388
= <86667626+ibrahim-atef@users.noreply.github.com>
2024-10-17 06:35:49 +0300

    series logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,47 @@ import HeroSlide from "../coponents/utilitiesCpmponents/hero-slide/HeroSlide";
 import { OutlinedButton } from "../coponents/utilitiesCpmponents/button/Button";
 import MovieList from "../coponents/utilitiesCpmponents/movieList/MovieList";
 import { category, movieType } from "../api/tmdbApi";
+import customApi from "../api/tmdbApi"; 
+import { useEffect, useState } from "react";
+
+
+
 const Home = () => {
+  const [noData, setnoData] = useState(false);
+const fetchData = async (apiMethod, params) => {
+  try {
+    const response = await customApi[apiMethod](params);
+    // Check for empty data scenarios
+    if (
+      (response.totalMovies === 0 && response.movies.length === 0) ||
+      (response.totalSeries === 0 && response.series.length === 0)
+    ) setnoData(true);
+    return response;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    setnoData(true);
+    return { error: true };
+  }finally{
+    setnoData(true);
+  }
+};
+
+useEffect(() => {
+  fetchData();
+}, []);
+
+  if (noData) {
+    return   <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "80vh",
+    }}
+  >
+    <p style={{ fontSize: "1.5rem", color: "#999" }}>No data to show</p>
+  </div>;
+  } else {
   return (
     <div>
       <HeroSlide />
@@ -46,7 +86,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-  );
+  );}
 };
 
 export default Home;
